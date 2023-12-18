38ae4966d950b594af1958f385f790da53f14c2e
yeungemy <emyzheng80@gmail.com>
2023-12-18 13:56:26 -0500

    updated yaml file

--- cypress/e2e/ui/test/functionTest/homePage/header.spec.ts
+++ cypress/e2e/ui/test/functionTest/homePage/header.spec.ts
@@ -10,6 +10,7 @@ describe("Page Header Bar", () => {
     it("should be able to see all tabs", () => {
         //verify all labels of tabs are displaying properly
         cy.get(header.selectors.NAV_LINKS).each(($navLink, index) => {
+            console.log("test");
             cy.wrap($navLink).should("contain.text", (Object.values(TABS.TABS))[index]);
         });
     }); 
