8a2742d24b2a411c559ea5965b3e49f7f1bb0de6
yeungemy <emyzheng80@gmail.com>
2023-12-24 23:29:10 -0500

    combined test cases

--- cypress/e2e/ui/test/functionTest/homePage/filters.spec.ts
+++ cypress/e2e/ui/test/functionTest/homePage/filters.spec.ts
@@ -2,6 +2,7 @@ import { filterPanel } from '../../../pages/homePage/filterPanel.section';
 import { cardContainer } from '../../../pages/homePage/carContainer.section';
 import { toolDetails } from '../../../pages/homePage/toolDetails/toolDetails.page';
 import * as toolbox from '../../../../../support/toolbox';
+import * as FILTER_PANEL from "../../../../data/FilterPanel";
 
 describe('Filters', () => {
   let indexOfFilter = 0;
@@ -10,29 +11,36 @@ describe('Filters', () => {
     cy.visit(Cypress.env('url'));
   });
 
+  it("should be able to see all sections of filter panel", () => {
+    //verify all section of filters panel are displaying
+    filterPanel.allGridTitles.each(($gridTitle, index) => {
+      cy.wrap($gridTitle).should("contain.text", Object.values(FILTER_PANEL.FILTER_PANEL)[index]);
+    });
+  });
+
   it("should be able to filter tools", () => {
     // Select a filter by index
     cy.get(filterPanel.selectors.FILTER_CHECKBOX).its('length').then(qtyOfCheckboxes => {
       // Select index of a filter
-      indexOfFilter = toolbox.generateRandomNumber(0, qtyOfCheckboxes - 1 );
+      indexOfFilter = toolbox.generateRandomNumber(0, qtyOfCheckboxes - 1);
 
       //extract filter label by index
       filterPanel.getLabelOfFilterByIndex(indexOfFilter).then(labelOfFilter => {
         //select filter by index
         filterPanel.selectFilterByIndex(indexOfFilter).then($filterResult => {
-            const numberOfCardsFiltered = $filterResult.find(cardContainer.selectors.CARD).length;
+          const numberOfCardsFiltered = $filterResult.find(cardContainer.selectors.CARD).length;
 
-            if (numberOfCardsFiltered) {
-              const indexOfCard = toolbox.generateRandomNumber(0, numberOfCardsFiltered - 1 );
+          if (numberOfCardsFiltered) {
+            const indexOfCard = toolbox.generateRandomNumber(0, numberOfCardsFiltered - 1);
 
-              // Open tool details page
-              cy.getElementByIndex(cardContainer.selectors.CARD, indexOfCard).click();
+            // Open tool details page
+            cy.getElementByIndex(cardContainer.selectors.CARD, indexOfCard).click();
 
-              // Verify the filter label is shown
-              cy.waitUntilTextToBeVisible(toolDetails.selectors.BADGE, labelOfFilter, 15000);
-            } else {
-              cy.get(cardContainer.selectors.NO_RESULTS_FILTERED).should('be.visible');
-            }
+            // Verify the filter label is shown
+            cy.waitUntilTextToBeVisible(toolDetails.selectors.BADGE, labelOfFilter, 15000);
+          } else {
+            cy.get(cardContainer.selectors.NO_RESULTS_FILTERED).should('be.visible');
+          }
         });
       });
     });
