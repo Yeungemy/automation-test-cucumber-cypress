895012b1e9628bffef4dbd1e199247dba5c61639
yeungemy <emyzheng80@gmail.com>
2023-12-24 19:17:03 -0500

    added new test case test pagination

--- cypress/e2e/ui/test/functionTest/homePage/pagination.spec.ts
+++ cypress/e2e/ui/test/functionTest/homePage/pagination.spec.ts
@@ -3,6 +3,9 @@ import * as PAGINATION from "../../../../data/Pagination";
 
 describe("Pangination", () => {
     const activePageNo = 1;
+    const indexOfMinPageNo = 0;
+    const indexOfMaxPageNo = 4;
+    let indexOfPaginationIcon: number = 0;
 
     beforeEach(() => {
         cy.visit(Cypress.env('url'));
@@ -15,7 +18,7 @@ describe("Pangination", () => {
             console.log(expectedResult);
 
             //determine the expected result
-            if (index > 0 && index < 4) {
+            if (index > indexOfMinPageNo && index < indexOfMaxPageNo) {
                 expectedResult = index.toString();
             } else {
                 expectedResult = index > 0 ? Object.values(PAGINATION.PAGINATION)[1] : Object.values(PAGINATION.PAGINATION)[0];
@@ -27,6 +30,8 @@ describe("Pangination", () => {
     });
 
     it("Should see inactive previous icon if it is on the page one", () => {
+        indexOfPaginationIcon = 4; 
+
         // find and filter the active page
         pagination.activePageLink.should('have.length', 1).and('contain.text', activePageNo.toString());
 
@@ -34,17 +39,32 @@ describe("Pangination", () => {
         pagination.getPageLinkByIndex().should('have.class', 'disabled');
 
         // verify the next page link is active
-        pagination.getPageLinkByIndex(4).should('not.have.class', 'disabled');
+        pagination.getPageLinkByIndex(indexOfPaginationIcon).should('not.have.class', 'disabled');
     });
 
-    it("Should be able to navigate to a new page", () => {
+    it("Should be able to navigate to a new page by clicking next icon", () => {
+        indexOfPaginationIcon = 4; 
+
         //verify current active page
         pagination.activePageLink.should('have.length', 1).and('contain.text', activePageNo.toString());
 
         //navigate to next page
-        pagination.getPageLinkByIndex(4).click();
+        pagination.getPageLinkByIndex(indexOfPaginationIcon).click();
 
         //verify current active page
         pagination.activePageLink.should('have.length', 1).and('not.contain.text', activePageNo.toString()).and('contain.text', (activePageNo + 1).toString());
+    });
+
+    it("Should be able to navigate to a new page by clicking number icon", () => {
+        indexOfPaginationIcon = 3; 
+
+        //verify current active page
+        pagination.activePageLink.should('have.length', 1).and('contain.text', activePageNo.toString());
+
+        //navigate to next page
+        pagination.getPageLinkByIndex(indexOfPaginationIcon).click();
+
+        //verify current active page
+        pagination.activePageLink.should('have.length', 1).and('not.contain.text', activePageNo.toString()).and('contain.text', indexOfPaginationIcon.toString());
     });
 });
